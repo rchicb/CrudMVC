a0fe91b6a54a2845fc37a81cb04361d5dea2f857
Raul Bulux <marachbu@gmail.com>
2020-04-12 23:59:47 -0500

    Creacion de Opcion subir archivo a un directorio dentro del servidor, temporalmente 2 archivos

---
+++ CrudMVC/Controllers/FileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudMVC.Models.ViewModels;
+
+namespace CrudMVC.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            if (TempData["Message"]!=null)
+            {
+                ViewBag.Message = TempData["Message"].ToString();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveFiles(FileViewModel model)
+        {
+
+            string rootPath=Server.MapPath("~/");
+
+            string pathFile1 = Path.Combine(rootPath + "/Files/archivo1.png");
+            string pathFile2 = Path.Combine(rootPath + "/Files/archivo2.png");
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index",model);
+            }
+            model.Archivo1.SaveAs(pathFile1);
+            model.Archivo2.SaveAs(pathFile2);
+
+            TempData["Message"] = "Se cargaron los archivos exitosamente";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CrudMVC/Models/ViewModels/FileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudMVC.Models.ViewModels
+{
+    public class FileViewModel
+    {
+        [Required(ErrorMessage ="El campo es obligatorio")]
+        [DisplayName("Seleccione un Archivo")]
+        public HttpPostedFileBase Archivo1 { get; set; }
+
+        [Required(ErrorMessage ="El campo es obligatorio")]
+        [DisplayName("Seleccione un Archivo")]
+        public HttpPostedFileBase Archivo2 { get; set; }
+        
+        [Required(ErrorMessage ="Campo cadena Obligatorio")]
+        [DisplayName("Agregar Cadena")]
+        public string Cadena { get; set; }
+    }
+}
